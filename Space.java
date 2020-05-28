d598d70c86cf008969337ae1fc1dc341eb381a32
Millie <47400469+Minnie-Million@users.noreply.github.com>
2020-05-28 11:58:00 -0700

    Added Space world class

---
+++ Space.java
@@ -0,0 +1,78 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Space here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Space extends World
+{
+    private Star[] stars = new Star[210];
+    Color color1 = new Color(229,227,246);
+    Color color2 = new Color(226,240,238);
+    Color color3 = new Color(241,226,242);
+
+    /**
+     * Constructor for objects of class Space.
+     * 
+     */
+    public Space()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        GreenfootImage image = getBackground();
+        image.setColor(Color.BLACK);
+        image.fill();
+        star();
+        prepare();
+    }
+    
+    private void star()
+    {
+        for(int i = 0; i < 210; i++)
+        {
+            Star star;
+            int deltaSpeed = Greenfoot.getRandomNumber(2);
+            
+            if(i < 70)
+            {
+                star = new Star(-1 - deltaSpeed, color1, getWidth(), getHeight());
+                addObject(star, star.getX(), star.getY());
+                stars[i] = star;
+            }
+
+            if(i >= 70 && i < 140)
+            {
+                star = new Star(-3 - deltaSpeed, color2, getWidth(), getHeight());
+                addObject(star, star.getX(), star.getY());
+                stars[i] = star;
+            }
+
+            if(i >= 140)
+            {
+                star = new Star(-5 - deltaSpeed, color3, getWidth(), getHeight());
+                addObject(star, star.getX(), star.getY());
+                stars[i] = star;
+            }
+        }
+    }
+    
+    public void act()
+    {
+        
+    }
+    
+    private void prepare()
+    {
+        Echo echo = new Echo();
+        addObject(echo,123,210);
+        Drone drone = new Drone();
+        addObject(drone,479,157);
+        Drone drone2 = new Drone();
+        addObject(drone2,427,206);
+        Drone drone3 = new Drone();
+        addObject(drone3,479,255);
+    }
+}
