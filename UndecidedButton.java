38fcd50f6bc1d141771263e95d438fcae8d1c5f7
Millie <47400469+Minnie-Million@users.noreply.github.com>
2020-05-29 10:42:44 -0700

    working on getting the undecided button to disappear when other buttons are pressed

--- UndecidedButton.java
+++ UndecidedButton.java
@@ -1,19 +1,63 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class UndecidedButton here.
+ * Write a description of class EngineerButton here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class UndecidedButton extends Actor
 {
-    /**
-     * Act - do whatever the UndecidedButton wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
+    private static final Color transparent = new Color(0,0,0,0);
+    private GreenfootImage background;
+    private String prefix;
+    public GreenfootSound undecided;
+    private boolean next = false;
+    private GreenfootSound drones;
+    
+    public UndecidedButton()
     {
-        // Add your action code here.
-    }    
+        this("I don't know yet...");
+    }
+
+    public UndecidedButton(String prefix)
+    {
+        background = getImage();  // get image from class
+        this.prefix = prefix;
+        updateImage();
+        undecided = new GreenfootSound("undecided.mp3");
+        drones = new GreenfootSound("Opening Scene 2.mp3");
+    }
+    
+    /**
+     * Update the image on screen to show the current value.
+     */
+    private void updateImage()
+    {
+        GreenfootImage image = new GreenfootImage(background);
+        GreenfootImage text = new GreenfootImage(prefix, 22, Color.BLACK, transparent);
+        
+        if (text.getWidth() > image.getWidth() - 20)
+        {
+            image.scale(text.getWidth() + 20, image.getHeight());
+        }
+        
+        image.drawImage(text, (image.getWidth()-text.getWidth())/2, 
+                        (image.getHeight()-text.getHeight())/2);
+        setImage(image);
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(this) && !undecided.isPlaying())
+        {
+            undecided.play();
+            getWorld().removeObject(this);
+        }
+        
+        if (Greenfoot.mouseClicked(EngineerButton.class) || Greenfoot.mouseClicked(SoldierButton.class))
+        {
+            getWorld().removeObject(this);
+        }
+    }
 }
