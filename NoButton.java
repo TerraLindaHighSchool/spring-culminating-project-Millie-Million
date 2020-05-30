a6c2c8a5bbe1a8035cb76d2dfb1afada621ef8a8
Millie <47400469+Minnie-Million@users.noreply.github.com>
2020-05-29 17:02:42 -0700

    No button exists and plays narration

--- NoButton.java
+++ NoButton.java
@@ -1,19 +1,56 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class NoButton here.
+ * Write a description of class EngineerButton here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class NoButton extends Actor
 {
-    /**
-     * Act - do whatever the NoButton wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
+    private static final Color transparent = new Color(0,0,0,0);
+    private GreenfootImage background;
+    private String prefix;
+    private boolean next = false;
+    public GreenfootSound no;
+    
+    public NoButton()
     {
-        // Add your action code here.
-    }    
+        this("No");
+    }
+
+    public NoButton(String prefix)
+    {
+        background = getImage();  // get image from class
+        this.prefix = prefix;
+        updateImage();
+        no = new GreenfootSound("no.mp3");
+    }
+    
+    /**
+     * Update the image on screen to show the current value.
+     */
+    private void updateImage()
+    {
+        GreenfootImage image = new GreenfootImage(background);
+        GreenfootImage text = new GreenfootImage(prefix, 22, Color.BLACK, transparent);
+        
+        if (text.getWidth() > image.getWidth() - 20)
+        {
+            image.scale(text.getWidth() + 20, image.getHeight());
+        }
+        
+        image.drawImage(text, (image.getWidth()-text.getWidth())/2, 
+                        (image.getHeight()-text.getHeight())/2);
+        setImage(image);
+    }
+    
+    public void act()
+    {
+            if (Greenfoot.mouseClicked(this) && !no.isPlaying())
+            {
+                no.play();
+                getWorld().removeObject(this);
+            }
+    }
 }
