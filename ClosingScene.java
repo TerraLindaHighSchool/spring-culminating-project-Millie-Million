8053144810c6fea7ccd9ff3357b7cd0eed54295b
Millie <47400469+Minnie-Million@users.noreply.github.com>
2020-06-04 21:00:38 -0700

    Added narration and timer to closeing scene world

--- ClosingScene.java
+++ ClosingScene.java
@@ -8,6 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class ClosingScene extends World
 {
+    private int time = 0;
+    private int count;
+    private GreenfootSound goodbye = new GreenfootSound("Win scenario2.mp3");
 
     /**
      * Constructor for objects of class ClosingScene.
@@ -17,5 +20,37 @@ public class ClosingScene extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        prepare();
+    }
+    
+    private void prepare()
+    {
+        goodbye.play();
+    }
+    
+    public void act()
+    {
+        setTime();
+        endsAreBeginnings();
+    }
+    
+    public void setTime()
+    {
+        if (count++ % 60 == 0)
+        {
+            if (time >= 0)
+            {
+                time++;
+            }
+        }
+    }
+    
+    public void endsAreBeginnings()
+    {
+        if (time % 33 == 0)
+        {
+            Greenfoot.setWorld(new EndGameWin());
+        }
     }
 }
