c39829ecef411347cb12e8b82db9d09fa19f3406
Millie <47400469+Minnie-Million@users.noreply.github.com>
2020-06-05 21:54:07 -0700

    added stationary Calix cuz he doesn't move in the last 2 scenes

---
+++ Calix2.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Calix2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Calix2 extends Actor
+{
+    private int time = 0;
+    private int count;
+    
+    private GreenfootImage calixLeft = new GreenfootImage("Calix2.png");
+    private GreenfootImage calixBack = new GreenfootImage("Calix3.png");
+    private GreenfootImage calixRight = new GreenfootImage("Calix4.png");
+    private GreenfootImage calixFront = new GreenfootImage("Calix1.png");
+    
+    /**
+     * Act - do whatever the Calix2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        
+    }
+}
